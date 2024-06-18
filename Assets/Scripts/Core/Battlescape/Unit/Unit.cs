7ecafc681b1b3be07cab504cc834168238f156ec
The1Wolfcast <the1wolfcast@gmail.com>
2024-06-17 21:45:53 -0500

    done for the night

---
+++ Assets/Scripts/Core/Battlescape/Unit/Unit.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    #region References
+
+
+
+    #endregion
+
+    //Stats
+    [SerializeField] private string Name;
+
+    #region Combat Stats
+
+    [SerializeField] private int TimeUnits;
+
+    #endregion
+
+
+}
